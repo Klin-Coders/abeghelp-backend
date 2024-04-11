56973354e90cb20c51695097d84c36e4e3b2f783
JC-Coder <josephchimezie2003@gmail.com>
2024-04-11 20:29:08 +0100

    update seeders

--- src/scripts/seeders/index.ts
+++ src/scripts/seeders/index.ts
@@ -1,9 +1,10 @@
 import { faker } from '@faker-js/faker';
 import mongoose from 'mongoose';
-import { Country, FundraiserEnum, StatusEnum } from '../../common/constants';
-import { campaignModel } from '../../models';
+import { Category, Country, FundraiserEnum, Gender, Provider, StatusEnum } from '../../common/constants';
+import { UserModel, campaignCategoryModel, campaignModel } from '../../models';
 import { ENVIRONMENT } from '../../common/config';
 import { customAlphabet } from 'nanoid';
+import { ICampaignCategory, IUser } from '../../common/interfaces';
 
 const nanoid = customAlphabet('123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ', 6);
 
@@ -12,10 +13,57 @@ async function seedCampaigns(size?: number) {
 	try {
 		const campaignsToSeed: unknown[] = [];
 
+		let creator: IUser | null = null;
+		let category: ICampaignCategory | null = null;
+
 		for (let i = 0; i < size! ?? 100; i++) {
+			if (i % 3 === 0) {
+				try {
+					creator = await UserModel.create({
+						firstName: faker.internet.userName(),
+						lastName: faker.internet.userName(),
+						email: faker.internet.email(),
+						password: faker.internet.password(),
+						phoneNumber: faker.phone.number(),
+						isProfileComplete: true,
+						provider: Provider.Local,
+						isTermAndConditionAccepted: true,
+						gender: Gender.Male,
+					});
+				} catch (error) {
+					console.log(`Error creating creator ${error}`);
+				}
+			}
+
+			if (!creator) {
+				console.log('Unable to create creator');
+				continue;
+			}
+
+			if (i % 2 === 0) {
+				const categoryName = faker.helpers.arrayElement(Object.values(Category));
+
+				category = await campaignCategoryModel.findOneAndUpdate(
+					{
+						name: categoryName,
+					},
+					{
+						name: categoryName,
+						isDeleted: false,
+						image: faker.image.avatar(),
+					},
+					{ upsert: true, new: true }
+				);
+			}
+
+			if (!category) {
+				console.log('Unable to create category');
+				continue;
+			}
+
 			const newCampaign = {
 				url: `${ENVIRONMENT.FRONTEND_URL}/c/${nanoid()}`,
-				category: new mongoose.Types.ObjectId(), // Generate a fake ObjectId
+				category: category!['_id'],
 				country: faker.helpers.arrayElement(Object.values(Country)),
 				tags: [faker.lorem.word(), faker.lorem.word()], // Generate random tags
 				title: faker.lorem.words(10),
@@ -29,9 +77,9 @@ async function seedCampaigns(size?: number) {
 						blurHash: faker.image.urlPlaceholder(),
 					},
 				],
-				story: faker.lorem.paragraph(200),
-				storyHtml: faker.lorem.paragraphs(200),
-				creator: new mongoose.Types.ObjectId(),
+				story: faker.lorem.paragraph(20),
+				storyHtml: faker.lorem.paragraphs(20),
+				creator: creator!['_id'],
 				status: StatusEnum.APPROVED,
 				isFlagged: faker.datatype.boolean(),
 				flaggedReasons: [],
