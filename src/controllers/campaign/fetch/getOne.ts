0b2052981fa78e9a1a8c62a07a43bcdb36827146
1finedev <49831574+1finedev@users.noreply.github.com>
2024-07-07 16:47:08 +0300

    pwned

--- src/controllers/campaign/fetch/getOne.ts
+++ src/controllers/campaign/fetch/getOne.ts
@@ -1,5 +1,5 @@
 import { ICampaign } from '@/common/interfaces';
-import { AppError, AppResponse, getDomainReferer, getFromCache, setCache } from '@/common/utils';
+import { AppError, AppResponse, getFromCache, setCache } from '@/common/utils';
 import { catchAsync } from '@/middlewares';
 import { campaignModel } from '@/models';
 import { Request, Response } from 'express';
@@ -13,17 +13,7 @@ export const getOneCampaign = catchAsync(async (req: Request, res: Response) => 
 		return AppResponse(res, 400, null, 'Please provide a campaign url');
 	}
 
-<<<<<<< HEAD
-	let cachedCampaign: Require_id<ICampaign> | null = null;
-
-	// check if request is from localhost
-	if (!getDomainReferer(req)?.includes('localhost')) {
-		// fetch from cache
-		cachedCampaign = await getFromCache<Require_id<ICampaign>>(shortId);
-	}
-=======
 	const cachedCampaign = await getFromCache<Require_id<ICampaign>>(shortId);
->>>>>>> 040f39f (refactor)
 
 	// fetch from DB if not previously cached
 	const campaign = cachedCampaign
